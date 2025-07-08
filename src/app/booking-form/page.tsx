e35d970c8962ca4b59ac2b771fb48edd1338cc96
shoebabedin <shoeb.abedin@intelli.global>
2025-07-08 19:07:11 +0600

    update

--- src/app/booking-form/page.tsx
+++ src/app/booking-form/page.tsx
@@ -14,7 +14,7 @@ const BookingForm = () => {
       additionalNotes: "",
     },
     validationSchema: Yup.object({
-      Name: Yup.string().required("Name is required"),
+      name: Yup.string().required("Name is required"),
       email: Yup.string().required("Email is required"),
       preferredDate: Yup.string().required("Preferred Date is required"),
       timeSlot: Yup.string().required("Time Slot is required"),
@@ -29,23 +29,95 @@ const BookingForm = () => {
       <div className="container">
         <div className="shadow-2xl max-w-[600px] mx-auto rounded-2xl py-5 px-8">
           <form onSubmit={formik.handleSubmit} className="grid grid-cols-12 gap-4">
-            <div className="col-span-12">
-               <label className="block mb-2 font-medium text-base md:text-xl leading-6 text-(--color-text)/70">
-                  First Name <span className="text-red-500">*</span>
-                </label>
-                <input
-                  onBlur={formik.handleBlur}
-                  onChange={formik.handleChange}
-                  value={formik.values.name}
-                  type="text"
-                  id="name"
-                  name="name"
-                  placeholder="First name"
-                  className="w-full bg-transparent text-(--color-text)/70 placeholder-gray-400 p-3 rounded-md border border-(--color-text)/70 focus:outline-none mb-4"
-                />
-                {formik.touched.name && formik.errors.name ? (
-                  <div className="text-danger">{formik.errors.name}</div>
-                ) : null}
+            <div className="col-span-12 mb-4">
+              <label className="block mb-2 font-medium text-base md:text-xl leading-6 text-(--color-text)/70">
+                Name <span className="text-red-500">*</span>
+              </label>
+              <input
+                onBlur={formik.handleBlur}
+                onChange={formik.handleChange}
+                value={formik.values.name}
+                type="text"
+                id="name"
+                name="name"
+                placeholder="First name"
+                className="w-full bg-transparent text-(--color-text)/70 placeholder-gray-400 p-3 rounded-md border border-(--color-text)/70 focus:outline-none mb-1"
+              />
+              {formik.touched.name && formik.errors.name ? (
+                <div className="text-red-600">{formik.errors.name}</div>
+              ) : null}
+            </div>
+            <div className="col-span-12 mb-4">
+              <label className="block mb-2 font-medium text-base md:text-xl leading-6 text-(--color-text)/70">
+                Email <span className="text-red-500">*</span>
+              </label>
+              <input
+                onBlur={formik.handleBlur}
+                onChange={formik.handleChange}
+                value={formik.values.email}
+                type="text"
+                id="email"
+                name="email"
+                placeholder="Enter your email"
+                className="w-full bg-transparent text-(--color-text)/70 placeholder-gray-400 p-3 rounded-md border border-(--color-text)/70 focus:outline-none mb-1"
+              />
+              {formik.touched.email && formik.errors.email ? (
+                <div className="text-red-600">{formik.errors.email}</div>
+              ) : null}
+            </div>
+            <div className="col-span-12 mb-4">
+              <label className="block mb-2 font-medium text-base md:text-xl leading-6 text-(--color-text)/70">
+                Preferred Date <span className="text-red-500">*</span>
+              </label>
+              <input
+                onBlur={formik.handleBlur}
+                onChange={formik.handleChange}
+                value={formik.values.preferredDate}
+                type="text"
+                id="preferredDate"
+                name="preferredDate"
+                placeholder="First name"
+                className="w-full bg-transparent text-(--color-text)/70 placeholder-gray-400 p-3 rounded-md border border-(--color-text)/70 focus:outline-none mb-1"
+              />
+              {formik.touched.preferredDate && formik.errors.preferredDate ? (
+                <div className="text-red-600">{formik.errors.preferredDate}</div>
+              ) : null}
+            </div>
+            <div className="col-span-12 mb-4">
+              <label className="block mb-2 font-medium text-base md:text-xl leading-6 text-(--color-text)/70">
+                Time Slot <span className="text-red-500">*</span>
+              </label>
+              <input
+                onBlur={formik.handleBlur}
+                onChange={formik.handleChange}
+                value={formik.values.timeSlot}
+                type="text"
+                id="timeSlot"
+                name="timeSlot"
+                placeholder="First name"
+                className="w-full bg-transparent text-(--color-text)/70 placeholder-gray-400 p-3 rounded-md border border-(--color-text)/70 focus:outline-none mb-1"
+              />
+              {formik.touched.timeSlot && formik.errors.timeSlot ? (
+                <div className="text-red-600">{formik.errors.timeSlot}</div>
+              ) : null}
+            </div>
+            <div className="col-span-12 mb-4">
+              <label className="block mb-2 font-medium text-base md:text-xl leading-6 text-(--color-text)/70">
+               Additional Notes <span className="text-red-500">*</span>
+              </label>
+              <input
+                onBlur={formik.handleBlur}
+                onChange={formik.handleChange}
+                value={formik.values.additionalNotes}
+                type="text"
+                id="additionalNotes"
+                name="additionalNotes"
+                placeholder="First name"
+                className="w-full bg-transparent text-(--color-text)/70 placeholder-gray-400 p-3 rounded-md border border-(--color-text)/70 focus:outline-none mb-1"
+              />
+              {formik.touched.additionalNotes && formik.errors.additionalNotes ? (
+                <div className="text-red-600">{formik.errors.additionalNotes}</div>
+              ) : null}
             </div>
             <div className="col-span-12">
               <button
